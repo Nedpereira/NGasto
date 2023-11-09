366734926a3ee4a0e200968ffd4ed84c01d75a49
Nedpereira <neder_pereira@hotmail.com>
2023-11-09 20:40:09 -0300

    🎉 Projeto Iniciado

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet, View } from 'react-native';
+import Home from './src/Pages/Home';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Home/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
