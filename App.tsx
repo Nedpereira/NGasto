1e1765e920231b82077b1e57708dab636043b9b6
Nedpereira <neder_pereira@hotmail.com>
2023-11-16 22:59:51 -0300

    ✨ feat: add navigationContainer

--- App.tsx
+++ App.tsx
@@ -1,18 +1,39 @@
-import { StyleSheet, View } from 'react-native';
-import Home from './src/Pages/Home';
+import * as React from "react";
+import { StyleSheet, useColorScheme } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./src/Pages/Home";
+import { ThemeProvider } from "./src/Components/ThemeContext";
+import Inicial from "./src/Pages/Inicial";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const scheme = useColorScheme();
+
   return (
-    <View style={styles.container}>
-      <Home/>
-    </View>
+    <ThemeProvider>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="inicial" component={Inicial} />
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  lightContainer: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "white",
+  },
+  darkContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#121212",
   },
 });
