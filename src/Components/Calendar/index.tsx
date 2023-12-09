0afb9f485380575dea3bb4a1a23fd0e833018703
Nedpereira <need_vips@hotmail.com>
2023-12-09 10:13:57 -0300

    ✨ feat: add dispatch redux

--- src/Components/Calendar/index.tsx
+++ src/Components/Calendar/index.tsx
@@ -3,7 +3,7 @@ import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import Right from 'react-native-vector-icons/FontAwesome5';
 import moment from 'moment';
 import 'moment/locale/pt-br';
-import { mainColors } from '../../Styles/themes';
+import { calendarColors } from '../../Styles/themes';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { useDispatch } from 'react-redux';
 import { Calendar } from '../../Redux/Actions';
@@ -23,20 +23,20 @@ export const CustomCalendarHeader = () => {
 
     useEffect(() => {
         if (currentMonth) {
-            dispatch(Calendar(currentMonth.format('MMM'), currentMonth.format('YYYY')));
+            dispatch(Calendar(currentMonth.format('MMM'+ 'YYYY')));
         }
     }, [currentMonth]);
 
     return (
         <View style={styles.container}>
             <TouchableOpacity onPress={goToPreviousMonth}>
-                <Right name="angle-left" color="white" size={20} />
+                <Right name="angle-left" color="white" size={30} />
             </TouchableOpacity>
             <Text style={styles.calendarValue}>
                 {currentMonth.format('MMM')} <Text style={{ textTransform: 'lowercase' }}>de</Text> {currentMonth.format('YYYY')}
             </Text>
             <TouchableOpacity onPress={goToNextMonth}>
-                <Right name="angle-right" color="white" size={20} />
+                <Right name="angle-right" color="white" size={30} />
             </TouchableOpacity>
         </View>
     );
@@ -52,7 +52,7 @@ const styles = StyleSheet.create({
         textTransform: 'capitalize',
         fontSize: RFValue(14),
         fontFamily: 'Fredoka-Medium',
-        color: mainColors.text,
+        color: calendarColors.text,
         marginHorizontal: 20,
     },
 });
