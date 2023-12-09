93c09c7e5d05d5dbf58a59c0381720df6134ee41
Nedpereira <need_vips@hotmail.com>
2023-12-09 10:14:34 -0300

    ✨ feat: new structure balanceCard

--- src/Components/BalanceCard/index.tsx
+++ src/Components/BalanceCard/index.tsx
@@ -1,7 +1,9 @@
-import { StyleSheet, Text, Dimensions, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, Platform } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import Card from '../Card';
 import { RFValue } from 'react-native-responsive-fontsize';
+import { balanceColors } from '../../Styles/themes';
+
+const { width, height } = Dimensions.get('window');
 
 type BalanceCardProps = {
     balance?: number;
@@ -10,50 +12,55 @@ type BalanceCardProps = {
     icon: string;
 };
 
-const { width, height } = Dimensions.get('window');
-
-export const BalanceCard = ({ balance, name, type, icon }: BalanceCardProps) => {
+export const BalanceCard = ({ balance = 0, name, type, icon }: BalanceCardProps) => {
     const isProfit = type === 'Lucro';
     const color = isProfit ? '#28A745' : '#CA143F';
 
-    console.log(isProfit);
-
     return (
-        <Card style={styles.card}>
-            <View style={{ flexDirection: 'row', alignSelf: 'center', justifyContent: 'center'}}>
-                <View style={{alignSelf: 'center', width: 60, backgroundColor: 'red'}}>
-                    <Icon name={icon} size={32} color={color} />
-                </View>
-                <View style={{alignSelf: 'center' ,justifyContent: 'space-between',  height: 60}}>
-                    <View style={styles.content_balance}>
-                        <Text style={styles.balance}>R$</Text>
-                        <Text style={[styles.balance, { color }]}>{balance}</Text>
-                    </View>
-                    <View>
-                        <Text style={[styles.name]}>{name}</Text>
-                    </View>
-                </View>
+        <View style={styles.card}>
+            <View style={styles.iconContainer}>
+                <Icon name={icon} size={RFValue(26)} color={color} />
             </View>
-        </Card>
+            <View style={styles.textContainer}>
+                <Text style={[styles.balance, { color }]}>R$ {balance.toFixed(2)}</Text>
+                <Text style={styles.name}>{name}</Text>
+            </View>
+        </View>
     );
 };
 
 const styles = StyleSheet.create({
     card: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
         width: width * 0.42,
         height: height * 0.1,
-        backgroundColor: '#FFFFFF',
+        backgroundColor: balanceColors.backgroundColor,
         borderRadius: 8,
+        padding: 10,
     },
-    content_balance: {
-        flexDirection: 'row',
+    iconContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginRight: 10,
+    },
+    textContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'flex-start',
     },
     balance: {
-        fontSize: RFValue(14),
+        fontSize: RFValue(12),
         fontFamily: 'Fredoka-Medium',
+        alignSelf: 'center',
     },
     name: {
-        fontSize: RFValue(16),
+        fontSize: RFValue(12),
         fontFamily: 'Fredoka-Medium',
+        color: balanceColors.text,
+        alignSelf: 'center',
     },
 });
+
+export default BalanceCard;
